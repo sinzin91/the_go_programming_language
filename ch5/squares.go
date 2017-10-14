d81a9e2ef5baa73f6d5c6f2decd057fe01adddfb
Tenzin <sinzin91@gmail.com>
2017-10-14 13:23:50 -0700

    squares.go

---
+++ ch5/squares.go
@@ -0,0 +1,23 @@
+// squares returns a function that returns 
+// the next square number each time it is called
+package main
+
+import "fmt"
+
+func squares() func() int {
+	var x int
+	return func() int {
+		x++
+		return x * x
+	}
+}
+
+func main() {
+	f := squares()
+	fmt.Println(f()) // "1"
+	fmt.Println(f()) // "4"
+	fmt.Println(f()) // "9"
+	fmt.Println(f()) // "16"
+	x := squares()
+	fmt.Println(x()) // "1"
+}
